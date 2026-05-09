b28ddb03a01d7fc0f2dd205a0af1f8f2764ee472
Julia Guilhermino <juh.guilhermino03@gmail.com>
2026-05-09 11:51:08 -0300

    initial commit

---
+++ src/main/java/com/example/lyricsflow_backend/LyricsflowBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.lyricsflow_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LyricsflowBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LyricsflowBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lyricsflow_backend/LyricsflowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lyricsflow_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LyricsflowBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
