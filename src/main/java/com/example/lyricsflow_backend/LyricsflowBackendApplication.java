b0fd9bd4cd84725ad098596239c0e20dc2140ab9
Julia Guilhermino <juh.guilhermino03@gmail.com>
2026-05-13 01:38:52 -0300

    reset

--- src/main/java/com/example/lyricsflow_backend/LyricsflowBackendApplication.java
+++ src/main/java/com/example/lyricsflow_backend/LyricsflowBackendApplication.java
@@ -1,12 +1,13 @@
-// LyricsFlowApplication.java
 package com.example.lyricsflow_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LyricsFlowApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(LyricsFlowApplication.class, args);
-    }
+public class LyricsflowBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LyricsflowBackendApplication.class, args);
+	}
+
 }
